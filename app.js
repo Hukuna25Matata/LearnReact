d0b3969fbaefdad23ef833164d7d68332be046c7
Nagaraj Govindaraj <nagaraj25@go.byuh.edu>
2025-05-13 21:35:29 -1000

    react code

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ function MyButton() {
       <button>I'm a button</button>
     );
   }
-  
+
+
+export default function MyApp() {
+    return (
+      <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+    );
+  }
