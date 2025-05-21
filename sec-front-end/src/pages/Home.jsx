06189d0635785e24dc2ef7990e1eb9de69bbcce4
Nagaraj Govindaraj <nagaraj25@go.byuh.edu>
2025-05-21 01:59:54 -1000

    Step 1: Created new-tutorial folder using Vite for To-Do App

--- sec-front-end/src/pages/Home.jsx
+++ sec-front-end/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 function Home() {
+
+    const handleClick = (e) => {
+        console.log('hello,raj', e);
+    }
+
     return (
       <div className="page">
         <h1>Welcome to the Cricket Info App</h1>
         <p>This app helps you learn about cricket and track match scores.</p>
+        ,<button onClick={handleClick}>click me</button>
       </div>
     );
   }
